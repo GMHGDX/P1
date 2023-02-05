2ccf984e753157eef77a6e32615abb03fcc98963
GabrielleHieken <gabriellehieken@gmail.com>
2023-02-05 16:50:00 -0600

    adding wait function

--- oss.c
+++ oss.c
@@ -65,8 +65,9 @@ int main(int argc, char *argv[]){
         }
         else /* parent code */
             printf("I am parent %ld I created %ld\n", (long)getpid(), (long)childpid);
-            childpid = wait(NULL);
+
             if(i == proc){
+                childpid = wait(NULL);
                 if (childpid != -1){
                     printf("Waited for child with pid %ld\n", childpid);
                 }

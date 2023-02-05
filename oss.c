ce4bd268752053e9842a87424461696fda1b6462
GabrielleHieken <gabriellehieken@gmail.com>
2023-02-04 18:35:04 -0600

    Line parsing

--- oss.c
+++ oss.c
@@ -2,8 +2,36 @@
 #include<stdio.h>
 
 int main(int argc, char *argv[]){
-	printf("hello world");
+	printf("You are in oss.c");
+	
+	int proc, simul, iter;
 
+	char opt;
+    while((opt = getopt(argc, argv, "hnst:")) != -1 )
+    {
+        switch (opt)
+        {
+        case 'h':
+			printf("This is your help message!");
+            break;
+        case 'n':
+            proc = atoi(optarg);
+			printf("proc/n: " + proc);
+            break;
+        case 's':
+            simul = atoi(optarg);
+			printf("simul/s: " + proc);
+            break;
+        case 't':
+            iter = atoi(optarg);
+			printf("iter/t: " + proc);
+            break;
+        default: /* '?' */
+            printf ("Invalid option %c\n", optopt);
+            print_usage (argv[0]);
+            return (EXIT_FAILURE);
+        }
+    }
 return 0;
 }
 

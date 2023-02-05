391f0ac58496355223600cbd62bf37c3d0f262a0
GabrielleHieken <gabriellehieken@gmail.com>
2023-02-05 14:57:13 -0600

    declare i outside of loop

--- worker.c
+++ worker.c
@@ -7,8 +7,9 @@ int worker(int iter){
 	printf("You are in worker.c \n");
 
     printf("Amount of loops: %i \n", iter);
+    int i;
 
-    for(int i = 1; i < (iter + 1); i++){
+    for(i = 1; i < (iter + 1); i++){
         //EX: WORKER PID:6577 PPID:6576 Iteration:3 before sleeping
         printf("WORKER PID: %i PPID: %i Iteration: %i before sleeping \n", 1, 2, i);
         sleep(5);

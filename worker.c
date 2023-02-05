0eb5bedb4af443385ec72a931d15c1010c9aca65
GabrielleHieken <gabriellehieken@gmail.com>
2023-02-04 19:28:16 -0600

    taking command line input for worker

--- worker.c
+++ worker.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-int main(int argc, char *argv[]){
+int worker(int argc, char *argv[]){
 	printf("You are in worker.c\n");
     if(argc == 2){
         char *n = argv[1];

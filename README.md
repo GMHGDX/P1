627b637c2d99490c407be8c34052b868d26f0f03
GabrielleHieken <gabriellehieken@gmail.com>
2023-02-06 21:03:16 -0600

    Created help message

--- README.md
+++ README.md
@@ -3,12 +3,7 @@ Class: 4760 Operating Systems
 
 Project - P1: 
 
-Shouldn't get any errors or weird bugs while running this project. Only got the warning below when creating a modulus function: 
-
-    warning: implicit declaration of function ‘Mod’ [-Wimplicit-function-declaration]
-             if(Mod(i, simul) == 0)
-
-All values (proc, simul, and iter) default to "1" if there it is not declared on command line.
+Shouldn't get any errors or weird bugs while running this project.
 
 Run the folowing command for help on how to run the project: 
 
--- oss.c
+++ oss.c
@@ -24,7 +24,15 @@ int main(int argc, char *argv[]){
         switch (opt)
         {
         case 'h':
-			printf("This is your help message!\n");
+			printf("To run this project: \n\n
+            run the command: ./oss -n num -s num -t num\n\n
+                    num = can be replaced with any number below 18 | -n, -s, and -t can be typed in any order\n
+                    -n = number of total children to launch\n
+                    -s = how many children run at the same time\n
+                    -t = number to pass to worker process (iterations)\n
+                    
+                    If you leave out a -n, -s, or -t it will defualt to the value 1\n\n
+                    Have fun :)\n");
             break;
         case 'n':
 			//atoi convert char to int
@@ -66,25 +74,14 @@ int main(int argc, char *argv[]){
             break;
         }
         else {
-            //wait for the process to finish after running the given simul int simultaneously
+            //wait for the process to finish after running the given simul int simultaneously before starting another process
             if(i >= simul){
                 wait(&stat);
             }
-
-            //wait again on the very last process if proc isnt evenly divisible by simul
-            // if(i == proc){
-            //     childpid = waitpid(childpid, &stat, 0);
-            // }
         }
     }
     return 0;
 }
-
-// //Modulus calculation in order to determine how many run simultaneously before doing wait function
-// int mod(int n, int d){
-//     int remainder = n - (d * ((int)(n/d)));
-//     return remainder;
-// }
 
 
 

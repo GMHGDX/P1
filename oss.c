52099c42782ff81898ddceb7b1d52d0480f92b49
GabrielleHieken <gabriellehieken@gmail.com>
2023-02-06 19:10:58 -0600

    oss handing off children to worker

--- oss.c
+++ oss.c
@@ -63,7 +63,7 @@ int main(int argc, char *argv[]){
             printf("I am child %ld and my parent is: %ld\n", (long)getpid(), (long)getppid());
             //worker(iter);
 
-            char *args[] = {"worker", (char)iter, NULL};
+            char *args[] = {"worker", NULL};
             execvp("./worker", args);
             printf("---------------------Returend with");
             break;
